9ba9cade10c165ae57f02de862a1e51bf340605f
polanskiy <polanskiy.html@gmail.com>
2019-11-08 18:33:56 +0300

    sass install

--- src/index.js
+++ src/index.js
@@ -11,17 +11,21 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.use(expressLayout);
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/home/home'));
+app.get('/', (req, res) => res.render('pages/home/home', {
+  sosi: 'kek',
+}));
 
 const live = livereload.createServer({
   exts: ['js', 'css', 'ejs'],
 });
 
 live.watch(path.join(__dirname, 'views'));
+live.watch(path.join(__dirname, 'public'));
 
 const port = process.env.PORT || 3001;
 
